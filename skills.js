4acab610f17b62ede4bc30190fb3390072d312e1
Louis-Bdc <82563026+Louis-Bdc@users.noreply.github.com>
2023-11-27 02:16:10 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,21 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+while(i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+function eatCat() {
+    console.log("I'm eating cat");
+}
+
+function loginScreen() {
+    username = "admin";
+    password = "admin";
+    if(username == "admin" && password == "admin") {
+        console.log("Login success");
+    } else {
+        console.log("Login failed");
+    }
+}
